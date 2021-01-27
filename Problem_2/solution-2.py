3f43be07d2dfb0e3685c1d6733eabec652446ed8
indlakesava <50535274+indlakesava@users.noreply.github.com>
2021-01-27 00:30:02 -0500

    Update solution-2.py

--- Problem_2/solution-2.py
+++ Problem_2/solution-2.py
@@ -11,3 +11,16 @@ print(product_lst([1,2,3,4,5]))
 
 #Without using division
 ----------------------------------------------------
+def product_lst(lst):
+    tmp_lst = []
+    for i in range(len(lst)):
+        product = 1
+        for j in range(i):
+            product *= lst[j]
+        for k in range(i+1, len(lst)):
+            product *= lst[k]
+        tmp_lst.append(product)
+        product=1
+    return tmp_lst
+
+print(product_lst([1,2,3,4,5]))
